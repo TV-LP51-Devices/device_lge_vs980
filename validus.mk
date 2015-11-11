e2f3a03161b1e29415c49800afacc35cef30d6b4
Adam <ovadia.adam@gmail.com>
2015-11-11 11:32:19 -0500

    fix merge derp

--- validus.mk
+++ validus.mk
@@ -1,14 +1,5 @@
-<<<<<<< HEAD:validus.mk
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 # Inherit some common Validus stuff.
 $(call inherit-product, vendor/validus/config/common_full_phone.mk)
-=======
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
->>>>>>> 27c985b5449cd7c07fce5e69b657e8e0fa667a04:cm.mk
 
 # Enhanced NFC
 $(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
